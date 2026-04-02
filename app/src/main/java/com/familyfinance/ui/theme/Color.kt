37c3675b9e0e31016e8868933e4c9424fba1ad84
Sergey Sorokin <seso@terraquantum.swiss>
2026-04-03 01:24:59 +0200

    Green color scheme

---
+++ app/src/main/java/com/familyfinance/ui/theme/Color.kt
@@ -0,0 +1,47 @@
+package com.familyfinance.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TertiaryGreen40 = Color(0xFF388E3C)
+val OnTertiaryGreen40 = Color(0xFFFFFFFF)
+val TertiaryGreenContainer90 = Color(0xFFC8E6C9)
+val OnTertiaryGreenContainer10 = Color(0xFF003300)
+
+val TertiaryGreen80 = Color(0xFF81C784)
+val OnTertiaryGreen80 = Color(0xFF003300)
+val TertiaryGreenContainer30 = Color(0xFF2E7D32)
+val OnTertiaryGreenContainer90 = Color(0xFFC8E6C9)
+
+// Neutrals
+val GreenBackgroundLight = Color(0xFFFBFDFB)
+val OnGreenBackgroundLight = Color(0xFF191C19)
+val GreenSurfaceLight = Color(0xFFFBFDFB)
+val OnGreenSurfaceLight = Color(0xFF191C19)
+val GreenSurfaceVariantLight = Color(0xFFDDE5DB)
+val OnGreenSurfaceVariantLight = Color(0xFF414941)
+
+val GreenBackgroundDark = Color(0xFF191C19)
+val OnGreenBackgroundDark = Color(0xFFE1E3DF)
+val GreenSurfaceDark = Color(0xFF191C19)
+val OnGreenSurfaceDark = Color(0xFFE1E3DF)
+val GreenSurfaceVariantDark = Color(0xFF414941)
+val OnGreenSurfaceVariantDark = Color(0xFFC1C9BF)
+val Green40 = Color(0xFF2E7D32)
+val OnGreen40 = Color(0xFFFFFFFF)
+val GreenContainer90 = Color(0xFFA5D6A7)
+val OnGreenContainer10 = Color(0xFF003300)
+
+val SecondaryGreen40 = Color(0xFF388E3C)
+val OnSecondaryGreen40 = Color(0xFFFFFFFF)
+val SecondaryGreenContainer90 = Color(0xFFC8E6C9)
+val OnSecondaryGreenContainer10 = Color(0xFF003300)
+
+val Green80 = Color(0xFF81C784)
+val OnGreen80 = Color(0xFF003300)
+val GreenContainer30 = Color(0xFF1B5E20)
+val OnGreenContainer90 = Color(0xFFA5D6A7)
+
+val SecondaryGreen80 = Color(0xFF81C784)
+val OnSecondaryGreen80 = Color(0xFF003300)
+val SecondaryGreenContainer30 = Color(0xFF2E7D32)
+val OnSecondaryGreenContainer90 = Color(0xFFC8E6C9)
